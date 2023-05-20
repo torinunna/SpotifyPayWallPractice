dbbec683741eda198ce8bdfcfce836e2874302a0
torinunna <torinunna9588@gmail.com>
2023-05-20 17:26:27 +0900

    update cell's appearance - cornerRadius

--- SpotifyPaywallPractice/BannerCell.swift
+++ SpotifyPaywallPractice/BannerCell.swift
@@ -14,6 +14,11 @@ class BannerCell: UICollectionViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var thumbnailImage: UIImageView!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 16
+    }
+    
     func configure(_ info: BannerInfo) {
         titleLabel.text = info.title
         descriptionLabel.text = info.description
